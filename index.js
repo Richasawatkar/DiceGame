fb2023fb1d6f5150e98b49b4fe51f637976438b6
Richa Sawatkar <127492462+Richasawatkar@users.noreply.github.com>
2023-12-13 18:47:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
-var randomNumber1=Math.random();
-randomNumber1=randomNumber1*6;
-randomNumber1=Math.floor(randomNumber1)+1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-if(randomNumber1===1){
-    document.querySelector(".img1").setAttribute("src","dice1.png");
-}
-if(randomNumber1===2){
-    document.querySelector(".img1").setAttribute("src","dice2.png");
-}
-if(randomNumber1===3){
-    document.querySelector(".img1").setAttribute("src","dice3.png");
-}
-if(randomNumber1===4){
-    document.querySelector(".img1").setAttribute("src","dice4.png");
-}
-if(randomNumber1===5){
-    document.querySelector(".img1").setAttribute("src","dice5.png");
-}
-if(randomNumber1===6){
-    document.querySelector(".img1").setAttribute("src","dice6.png");
+document.querySelector(".img1").setAttribute("src", "dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "dice" + randomNumber2 + ".png");
+
+var resultMessage = "";
+
+switch (true) {
+    case randomNumber1 > randomNumber2:
+        resultMessage = "🚩 Player 1 wins!";
+        break;
+    case randomNumber1 === randomNumber2:
+        resultMessage = "No one wins";
+        break;
+    default:
+        resultMessage = "Player 2 wins! 🚩";
 }
 
-var randomNumber2=Math.random();
-randomNumber2=randomNumber2*6;
-randomNumber2=Math.floor(randomNumber2)+1;
-
-if(randomNumber2===1){
-    document.querySelector(".img2").setAttribute("src","dice1.png");
-}
-if(randomNumber2===2){
-    document.querySelector(".img2").setAttribute("src","dice2.png");
-}
-if(randomNumber2===3){
-    document.querySelector(".img2").setAttribute("src","dice3.png");
-}
-if(randomNumber2===4){
-    document.querySelector(".img2").setAttribute("src","dice4.png");
-}
-if(randomNumber2===5){
-    document.querySelector(".img2").setAttribute("src","dice5.png");
-}
-if(randomNumber2===6){
-    document.querySelector(".img2").setAttribute("src","dice6.png");
-}
-
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="🚩 Player 1 wins!";
-}
-else if(randomNumber1===randomNumber2){
-    document.querySelector("h1").innerHTML="No one wins"
-}
-else{
-    document.querySelector("h1").innerHTML="Player 2 wins! 🚩";
-}
+document.querySelector("h1").innerHTML = resultMessage;
